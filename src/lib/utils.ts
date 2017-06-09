e789dd1f236dbddccaf9aef8fc2fab062a26f596
lckof-mac <suyu0925@gmail.com>
2017-06-09 23:11:01 +0800

    add some comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,12 +14,22 @@ export function combine(obj: any) {
   }).join('')
 }
 
+/**
+ * Return the signature of data
+ * @param json Data need sign
+ * @param key he api key
+ */
 export function getSignature(json: any, key: string) {
   const str = combine(json) + key
   const signature = crypto.createHash('md5').update(str, 'utf8').digest('hex').toLowerCase()
   return signature
 }
 
+/**
+ * Set 'sign' field to data, and return itself
+ * @param json Data need sign
+ * @param key the api key
+ */
 export function sign(json: any, key: string) {
   json.sign = getSignature(json, key)
   return json
